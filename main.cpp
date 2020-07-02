c79800fe176b88f815e5a6112e1cf57740107e00
supcicak0 <50813527+supcicak0@users.noreply.github.com>
2020-07-02 22:16:36 +0800

    minor edit

--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
+//NAME				: AAINAA NABILAH BINTI ROHAIZAD
+//MATRIC NO			: B031810373
+//SECTION/GROUP		: 2BITI S1G2
+//SUBMISSION DATE	: 1/6/2020
+
 #include <iostream>
 #include <ctime>
-#include <cmath>
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
 using namespace std;
-
 //declare constant - problem specification, population size
-const int GENE = 30;
+const int GENE = 8;
 const int CAPACITY = 104;
-const int POP_SIZE = 100;   //temporary
-const int PRICE[GENE] = { 8, 5, 6, 4, 13, 12, 5, 17, 10, 15, 9, 4, 6, 18, 8, 7, 7, 8, 8, 2, 9, 10, 10, 13, 7, 11, 12, 6, 21, 7 };
-const int TIME[GENE] = { 12, 11, 13, 9, 14, 14, 12, 20, 6, 21, 13, 9, 11, 15, 11, 9, 6, 7, 14, 6, 13, 8, 13, 16, 10, 10, 11, 10, 20, 10 };
-const float CO_probability = 0.9;
-const float MUT_probability = 0.9;
-const int MAX_GENERATION = 10;
-const int MAX_PRICE = 278;
-const int MAX_TIME = 120;
-const double PENALTY_MULTIPLIER = 0.25;
+const int POP_SIZE = 10;
+const int WEIGHT[GENE] = { 25, 35, 45, 5, 25, 3, 2, 2 };
+
 //declare chromosomes data structure
 int chromosome[POP_SIZE][GENE];
 //declare fitness data structure
 double fitness[POP_SIZE];
+
 int parents[2][GENE];
+
+const float CO_probability = 0.9;
+
 int children[2][GENE];
-//declare new data structure to hold new chromosomes
+
+const float MUT_PROBABILITY = 0.9;
+
 int newChromosome[POP_SIZE][GENE];
-int newChromosomeCounter = 0;
+
+int newChromosomeCounter=0;
+
+const int MAX_GENERATION = 100;
 
 double bestFitness = 9.9;
 double avgFitness = 0.0;
@@ -36,7 +42,8 @@ ofstream bestFitnessFile, avgFitnessFile, bestChromosomeFile;
 
 void initializePopulation() {
 	int randNum;
-
+	//initialize random seed
+	srand(time(NULL)); //SOURCE;http://www.cplusplus.com/reference/cstdlib/srand/
 	for (int c = 0; c < POP_SIZE; c++) {
 		for (int i = 0; i < GENE; i++) {
 			randNum = rand() % 2;
@@ -45,6 +52,14 @@ void initializePopulation() {
 	}
 }
 
+/*
+rand(): Returns a pseudo-random integral number in the range between 0 and RAND_MAX.
+RAND_MAX: This value is library-dependent, but is guaranteed to be at least 32767 on any standard library implementation.
+example:
+v1 = rand() % 100;         // v1 in the range 0 to 99
+v2 = rand() % 100 + 1;     // v2 in the range 1 to 100
+v3 = rand() % 30 + 1985;   // v3 in the range 1985-2014
+*/
 void printChromosome() {
 	for (int c = 0; c < POP_SIZE; c++) {
 		cout << "\tC" << c << "\t";
@@ -54,416 +69,236 @@ void printChromosome() {
 		cout << endl;
 	}
 }
-
 void evaluateChromosome() {
-	int accumulatedTime = 0;
-	int accumulatedPrice = 0;
-
-	cout << "\tC\t" << "Price\t" << "Time\t" << "Fitness\t" << endl;
-
+	int accumulatedWeight = 0;
 	for (int c = 0; c < POP_SIZE; c++) {
-		accumulatedTime = 0;
-		accumulatedPrice = 0;
-
+		accumulatedWeight = 0;
 		for (int i = 0; i < GENE; i++) {
 			if (chromosome[c][i] == 1) {
-				accumulatedTime += TIME[i];
-				accumulatedPrice += PRICE[i];
+				accumulatedWeight = accumulatedWeight + WEIGHT[i];
 			}
 		}
-
-		if (accumulatedTime <= MAX_TIME) {
-			fitness[c] = accumulatedPrice / (float)MAX_PRICE;
-		}
-		else {
-			fitness[c] = accumulatedPrice / (float)MAX_PRICE * PENALTY_MULTIPLIER;
-		}
-
-		cout << "\tC" << c << "\t" << accumulatedPrice << "\t" << accumulatedTime << "\t" << fitness[c] << endl;
+		fitness[c] = abs(CAPACITY - accumulatedWeight) / (float)CAPACITY;
+		cout << "\tC" << c << "\tDifference\t" << abs(CAPACITY - accumulatedWeight) << "\tFV\t" << fitness[c] << endl;
 	}
+
 }
-
 void parentSelection() {
-	double totalFitness = 0;
-	double pointer[2] = {0};
-	int indexParents[2];// = { 0 };
-	double temp = 0;
-	bool p1 = 0; //parent1 is selected flag
-	bool p2 = 0; //parent2 is selected flag
-
-	for (int c = 0; c < POP_SIZE; c++) {
-		totalFitness += fitness[c];
-	}
-
-	do
-	{
-		pointer[0] = {0};
-		pointer[1] = {0};
-		do {
-			for (int i = 0; i < 2; i++)
-			{
-				pointer[i] = 0 + static_cast <double> (rand()) / (static_cast <double> (RAND_MAX / (totalFitness - 0)));
-			}
-		} while (pointer[0] == pointer[1]);
-
-		temp = 0;
-		p1 = 0;
-		p2 = 0;
-
-		for (int c = 0; c < POP_SIZE; c++) {
-			temp += fitness[c];
-			//cout <<c<<" " << temp << endl;
-
-			if (p1 == 1 && p2 == 1) 
-			{
-				break;
-			}
-			else {
-				if (p1 == 0)
-				{
-					if (temp > pointer[0])
-					{
-						indexParents[0] = c;
-						p1 = 1;
-					}
-				}
-				if (p2 == 0)
-				{
-					if (temp > pointer[1])
-					{
-						indexParents[1] = c;
-						p2 = 1;
-					}
-				}
-			}
-		}
-
-	} while (indexParents[0] == indexParents[1]);
-
-	for (int p = 0; p < 2; p++)
-	{
-		cout << "Parent " << p + 1 << ": ";
-		for (int g = 0; g < GENE; g++)
-		{
-			parents[p][g] = chromosome[indexParents[p]][g];
-			cout << " " << parents[p][g];
-		}
-		cout << " Fitness= " << fitness[indexParents[p]] << endl;
-	}
-
-/*
-	double totalFitness = 0;
-	double pointer1 = 0;
-	double pointer2 = 0;
-	int indexParents[2] = { 0 };
-	double temp = 0;
-	bool p1 = 0; //parent1 is selected flag
-	bool p2 = 0; //parent2 is selected flag
-
-	for (int c = 0; c < POP_SIZE; c++) {
-		totalFitness += fitness[c];
-	}
+	int player1, player2;
+	int indexParents[2];
 
 	do {
 		for (int p = 0; p < 2; p++) {
+			player1 = rand() % POP_SIZE;
 			do {
-				pointer1 = fmod(rand(), totalFitness);
-				pointer2 = fmod(rand(), totalFitness);
+				player2 = rand() % POP_SIZE;
+			} while (player1 == player2);
 
-			} while (pointer1 == pointer2);
-		}
-
-		for (int c = 0; c < POP_SIZE; c++) {
-			temp += fitness[c];
-
-			if (p1 == 1 && p2 == 1) {
-				break;
+			if (fitness[player1] <= fitness[player2]) {
+				indexParents[p] = player1;
 			}
 			else {
-				if (temp > pointer1&& p1 == 0) {
-					indexParents[0] = c;
-					p1 = 1;
-				}
-
-				if (temp > pointer2&& p2 == 0) {
-					indexParents[1] = c;
-					p2 = 1;
-				}
+				indexParents[p] = player2;
 			}
+			cout << "\n\t Players: " << player1 << " vs " << player2;
+			cout << "\n\t Fitness: " << fitness[player1] << " vs " << fitness[player2];
+			cout << "\n\t Winner: " << indexParents[p];
+			cout << endl;
 		}
-
 	} while (indexParents[0] == indexParents[1]);
 
-	for (int p = 0; p < 2; p++)
-	{
-		cout << "Parent " << p + 1 << ": ";
-		for (int g = 0; g < GENE; g++)
-		{
+	for (int p = 0; p < 2; p++) {
+		cout << "\n\t Parents " << p + 1 << " : ";
+		for (int g = 0; g < GENE; g++) {
 			parents[p][g] = chromosome[indexParents[p]][g];
-			cout << " " << parents[p][g];
+			cout << parents[p][g]<<" ";
 		}
-		cout << " Fitness= " << fitness[indexParents[p]] << endl;
 	}
-*/
 
 }
-
 void crossover() {
-	float prob = 0;
-	int side = 0; //token for coin flip. 0 is heads, 1 is tails
+	float prob = 0.0;
+	int co_point;
+
+	for (int p = 0; p < 2; p++) {
+		for (int g = 0; g < GENE; g++) {
+			children[p][g] = parents[p][g];
+		}
+	}
 
 	prob = ((rand() % 10) + 1) / 10.0;
-
 	if (prob < CO_probability) {
-		for (int g = 0; g < GENE; g++)
-		{
-			side = rand() % 2;//0 to children 1, 1 to children 2
-			if (side == 0) {
-				children[0][g] = parents[0][g];
-				children[1][g] = abs(parents[0][g] - 1);
-			}
-			else {
-				children[1][g] = parents[1][g];
-				children[0][g] = abs(parents[1][g] - 1);
-			}
-		}
+		co_point = rand() % GENE;
+		cout << "\n\t Children crossover at " << co_point;
+		for (int g = co_point; g < GENE; g++) {
+			children[0][g] = parents[1][g];
+			children[1][g] = parents[0][g];
+		}		
 	}
 	else {
-		cout << "\nCrossover did not happen ";
+		cout << "\n\t Crossover did not happen";
 	}
 
-	for (int c = 0; c < 2; c++)
-	{
-		cout << "\nChildren" << c + 1 << ": ";
-		for (int g = 0; g < GENE; g++)
-		{
-			cout << children[c][g] << " ";
-		}
-	}
-}
-
-void mutation() {
-	float prob = 0;
-	int mut_point;
-	prob = (rand() % 11) / 10.0;
-
-
-	for (int c = 0; c < 2; c++)
-	{
-		if (prob < MUT_probability) {
-			mut_point = rand() % GENE;
-			cout << "\nMutation happened for Children " << c + 1 << " at mutation point " << mut_point;
-			if (children[c][mut_point] == 0) {
-				children[c][mut_point] = 1;
-			}
-			else {
-				children[c][mut_point] = 0;
-			}
-		}
-		else {
-			cout << "\nMutation did not happen for Children " << c + 1;
-		}
-	}
-
-	for (int c = 0; c < 2; c++)
-	{
-		cout << "\nChildren" << c + 1 << ": ";
-		for (int g = 0; g < GENE; g++)
-		{
-			cout << children[c][g] << " ";
-		}
-		
-	}
-	cout << endl;
-}
-
-void survivalSelection() {
-	int candidates[4][GENE];
-	int j = 0;
-
-	for (int i = 0; i < 4; i++) {
-		if (i < 2) {
-			for (int g = 0; g < GENE; g++)
-			{
-				candidates[i][g] = parents[i][g];
-			}
-		}
-		else {
-			for (int g = 0; g < GENE; g++)
-			{
-				candidates[i][g] = children[j][g];
-			}
-			j++;
-		}
-	}
-
-	int accumulatedTime = 0;
-	int accumulatedPrice = 0;
-	float tempFitness[4];
-	float tempBestFitness = 0;
-	int bestCandidateIndex[2];
-	int worstChromosomeIndex[2];
-	float tempWorstFitness = 100;
-
-	for (int c = 0; c < 4; c++) {
-		accumulatedTime = 0;
-		accumulatedPrice = 0;
-
-		for (int i = 0; i < GENE; i++) {
-			if (candidates[c][i] == 1) {
-				accumulatedTime += TIME[i];
-				accumulatedPrice += PRICE[i];
-			}
-		}
-
-		if (accumulatedTime <= MAX_TIME) {
-			tempFitness[c] = accumulatedPrice / (float)MAX_PRICE;
-		}
-		else {
-			tempFitness[c] = accumulatedPrice / (float)MAX_PRICE * PENALTY_MULTIPLIER;
-		}
-	}
-
-	for (int c = 0; c < 4; c++) {
-		if (tempFitness[c] >= tempBestFitness) {
-			tempBestFitness = tempFitness[c];
-			bestCandidateIndex[0] = c;
-		}
-	}
-
-	tempBestFitness = 0;
-
-	for (int c = 0; c < 4; c++) {
-		if (tempFitness[c] >= tempBestFitness && c != bestCandidateIndex[0]) {
-			tempBestFitness = tempFitness[c];
-			bestCandidateIndex[1] = c;
-		}
-	}
-
-	for (int c = 0; c < POP_SIZE; c++) {
-		if (fitness[c] <= tempWorstFitness) {
-			tempWorstFitness = fitness[c];
-			worstChromosomeIndex[0] = c;
-		}
-	}
-
-	tempWorstFitness = 100;
-
-	for (int c = 0; c < POP_SIZE; c++) {
-		if (fitness[c] <= tempWorstFitness && c != worstChromosomeIndex[0]) {
-			tempWorstFitness = fitness[c];
-			worstChromosomeIndex[1] = c;
-		}
-	}
-
-	for (int i = 0; i < 2; i++) {
+	//output
+	for (int c = 0; c < 2; c++) {
+		cout << "\n\t Children " << c + 1 << ":";
 		for (int g = 0; g < GENE; g++) {
-			chromosome[worstChromosomeIndex[i]][g] = candidates[bestCandidateIndex[i]][g];
+			cout << children[c][g]<<" ";
 		}
-		cout << "Replaced chromosome " << worstChromosomeIndex[i] << " with candidate " << bestCandidateIndex[i] << endl;
 	}
 }
+void mutation() {
+	float prob;
+	int mut_point;
 
+	for (int c = 0; c < 2; c++) {
+		prob = (rand() % 11) / 10.0;	//generating the prob value
+		if (prob < MUT_PROBABILITY) {
+			mut_point = rand() % GENE;
+			cout << "\n\t Mutation at gene = " << mut_point;
+			if (children[c][mut_point] == 0)
+				children[c][mut_point] = 1;
+			else
+				children[c][mut_point] = 0;
+		}
+		else {
+			cout << "\n\t Mutation did not happen.";
+		}
+	}
+	for (int c = 0; c < 2; c++) {
+		cout << "\n\tChildren: " << c + 1 << " after mutation: ";
+		for (int g = 0; g < GENE; g++) {
+			cout << children[c][g] << " ";
+		}
+	}
+}
+void survivalSelection() {
+
+	for (int c = 0; c < 2; c++) //copy children to new chromosome
+	{ 
+		for (int g = 0; g < GENE; g++) 
+		{
+			newChromosome[newChromosomeCounter][g] = children[c][g];
+		}
+		newChromosomeCounter++;
+	}
+	for (int c = 0; c < newChromosomeCounter; c++) //print the new chromosome
+	{
+		cout << "\n\t New chromosome " << c << " :";
+		for (int g = 0; g < GENE; g++)
+		{
+			cout << newChromosome[c][g] << " ";
+		}
+	}
+}
 void copyChromosome() {
 	for (int c = 0; c < POP_SIZE; c++)
 	{
 		for (int g = 0; g < GENE; g++)
 		{
-			newChromosome[c][g] = chromosome[c][g];
 			chromosome[c][g] = newChromosome[c][g];
 		}
 	}
 }
-
 void recordBestFitness() {
-	int bestChromosomeIndex;
-	for (int c = 0; c < POP_SIZE; c++) {
-		if (bestFitness > fitness[c]) {
+	for (int c = 0; c < POP_SIZE; c++)
+	{
+		if (bestFitness > fitness[c])
+		{
 			bestFitness = fitness[c];
-
-			for (int g = 0; g < GENE; g++) {
+			for (int g = 0; g < GENE; g++)
+			{
 				bestChromosome[g] = chromosome[c][g];
 			}
 		}
 	}
 
+	//output to console
 	cout << "\n Best Fitness = " << bestFitness;
 	cout << "\n Best Chromosome = ";
-	for (int g = 0; g < GENE; g++) {
+	for (int g = 0; g < GENE; g++)
+	{
 		cout << bestChromosome[g] << " ";
 	}
-	cout << endl;
 
-
+	//output to file
 	bestFitnessFile << bestFitness << endl;
-	for (int g = 0; g < GENE; g++) {
+	for (int g = 0; g < GENE; g++)
+	{
 		bestChromosomeFile << bestChromosome[g] << " ";
 	}
+
 	bestChromosomeFile << endl;
 }
-
 void calcAvgFitness() {
 	double sum = 0.0;
-	for (int c = 0; c < POP_SIZE; c++) {
-		sum += fitness[c]; //sum=sum+fitness
+	for (int c = 0; c < POP_SIZE; c++)
+	{
+		sum += fitness[c];
 	}
 	avgFitness = sum / POP_SIZE;
+
+	//output to console
 	cout << "\n Average Fitness = " << avgFitness;
 
+	//output to console
 	avgFitnessFile << avgFitness << endl;
 }
-
 int main() {
-	srand(time(NULL));
 
 	bestFitnessFile.open("bestFitness.txt");
 	avgFitnessFile.open("avgFitness.txt");
 	bestChromosomeFile.open("bestChromosome.txt");
 
-	int N;
-
-	cout << "\nENTER NUMBER OF GENERATION: ";
-	cin >> N;
-
-
 	cout << "\nGA START! \n";
-	cout << "First generation \n\n";
+
 	cout << "\nINITIALIZATION... \n";
 
+	//LAB 3
 	initializePopulation();
+	//getchar();
 
-	for (int g = 0; g < N; g++)
+	for (int g=0;g<MAX_GENERATION;g++) 
 	{
-		cout << "GENERATION " << g + 1 << endl;
-
+		cout << "GENERATION "<<g+1<<endl;
+		//LAB 3
 		cout << "\nPRINT POPULATION \n";
 		printChromosome();
+		//getchar();
 
+		//LAB 4
 		cout << "\nEVALUATE CHROMOSOME \n";
 		evaluateChromosome();
 		recordBestFitness();
 		calcAvgFitness();
+		//getchar();
 
-		for (int i = 0; i < POP_SIZE/2; i++)
+		for (int i = 0; i < POP_SIZE / 2; i++)
 		{
 			cout << "\nPARENT SELECTION \n";
 			parentSelection();
+			//getchar();
 
 			cout << "\nCROSSOVER \n";
 			crossover();
+			//getchar();
 
 			cout << "\nMUTATION \n";
 			mutation();
+			//getchar();
 
-			cout << "\nSURVIVAL SELECTION\n";
+			cout << "\nSURVIVAL SELECTION \n";
 			survivalSelection();
-			printChromosome();
+			//getchar();
 		}
-		cout << "\nCHROMOSOME COPIED TO NEW CHROMOSOME \n";
+		//for creating new chromosome
+		cout << "\nNEW CHROMOSOME COPIED TO CHROMOSOME \n";
 		copyChromosome();
+		//getchar();
 	}
-
+	
 	bestFitnessFile.close();
 	avgFitnessFile.close();
 	bestChromosomeFile.close();
+
 }
